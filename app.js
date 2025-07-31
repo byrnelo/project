a1fd45830e25b46bc48075f152e32c99ed54f84b
Suhayl Khan <suhaylmkhan@gmail.com>
2025-07-31 08:41:03 -0500

    editted query for Games to include game genre to showcase M:N relationship

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Lorraine Byrne and Suhayl Khan
 // CS 340 Summer 2025
 // Roll the Dice Database
-// Citations: 
+// Citations:
 // Structures and Setup:
 //https://canvas.oregonstate.edu/courses/2007765/assignments/10118865
 // https://canvas.oregonstate.edu/courses/2007765/pages/exploration-web-application-technology-2?module_item_id=25664612
@@ -43,10 +43,16 @@ app.get('/', async function (req, res) {
 
 app.get('/Games', async function (req, res) {
   try {
-    const query = `SELECT gameID, gameName, description, minPlayer, maxPlayer, quantity FROM Games;`;
+    const query = `
+        SELECT Games.gameID, Games.gameName, Games.description, Genres.genreName, Games.minPlayer, Games.maxPlayer, Games.quantity
+        FROM Games
+        LEFT OUTER JOIN GamesGenres ON Games.gameID = GamesGenres.gameID
+        LEFT OUTER JOIN Genres ON GamesGenres.genreID = Genres.genreID
+        ORDER BY Games.gameID;
+        `;
     const [games] = await db.query(query);
 
-    res.render('Games', { Games: games });  
+    res.render('Games', { Games: games });
   } catch (error) {
     console.error('Error executing queries:', error);
     res.status(500).send('An error occurred while executing the database queries.');
@@ -56,7 +62,7 @@ app.get('/Games', async function (req, res) {
 app.get('/Genres', async function (req, res) {
     try {
         const [genres] = await db.query('SELECT * FROM Genres;');
-        res.render('Genres', { Genres: genres }); 
+        res.render('Genres', { Genres: genres });
     } catch (error) {
         console.error('Error retrieving genres:', error);
         res.status(500).send('An error occurred while loading the Genres page.');
@@ -66,7 +72,7 @@ app.get('/Genres', async function (req, res) {
 app.get('/Tables', async function (req, res) {
     try {
         const [tables] = await db.query('SELECT * FROM Tables;');
-        res.render('Tables', { Tables: tables }); 
+        res.render('Tables', { Tables: tables });
     } catch (error) {
         console.error('Error retrieving tables:', error);
         res.status(500).send('An error occurred while loading the Tables page.');
@@ -76,7 +82,7 @@ app.get('/Tables', async function (req, res) {
 app.get('/Patrons', async function (req, res) {
     try {
         const [patrons] = await db.query('SELECT * FROM Patrons;');
-        res.render('Patrons', { Patrons: patrons }); 
+        res.render('Patrons', { Patrons: patrons });
     } catch (error) {
         console.error('Error retrieving patrons:', error);
         res.status(500).send('An error occurred while loading the Patrons page.');
@@ -86,7 +92,7 @@ app.get('/Patrons', async function (req, res) {
 app.get('/Reservations', async function (req, res) {
     try {
         const [reservations] = await db.query('SELECT * FROM Reservations;');
-        res.render('Reservations', { Reservations: reservations }); 
+        res.render('Reservations', { Reservations: reservations });
     } catch (error) {
         console.error('Error retrieving reservations:', error);
         res.status(500).send('An error occurred while loading the Reservations page.');
