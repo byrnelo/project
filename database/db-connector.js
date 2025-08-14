efc1e62741b20274aa4db3a97356b7b5d10502c7
byrnelo <byrnelo@engr.oregonstate.edu>
2025-08-14 20:58:10 +0000

    final with credentials taken out

--- database/db-connector.js
+++ database/db-connector.js
@@ -13,9 +13,9 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit   : 10,
     host              : 'classmysql.engr.oregonstate.edu',
-    user              : 'cs340_byrnelo',
-    password          : 'Kona101!',
-    database          : 'cs340_byrnelo'
+    user              : '',
+    password          : '',
+    database          : ''
 }).promise(); // This makes it so we can use async / await rather than callbacks
 
 // Export it for use in our application
